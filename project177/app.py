562d35c300fbe0e657a62f63e65b10620432ce32
Hunter17miao <118451474+Hunter17miao@users.noreply.github.com>
2024-06-07 17:43:39 +0800

    Add files via upload

---
+++ project177/app.py
@@ -0,0 +1,13 @@
+template = [
+    {
+        "inputs":5,
+        "category":"sports",
+        "word":"chess",
+    }
+    
+    {
+        "inputs":6,
+        "category":"Enter european country",
+        "word":"France",
+    }
+]
